64d0ad47ee9ad8dfcbf10335eccc8f5641162d6e
adambinch <abinch@sagarobotics.com>
2025-01-21 14:22:59 +0000

    fix

--- topological_navigation/src/topological_navigation/manager2.py
+++ topological_navigation/src/topological_navigation/manager2.py
@@ -1385,7 +1385,7 @@ class map_manager_2(object):
     def add_topological_nodes(self, data, update=True, write_map=True):
 
         for item in data:
-            success = self.add_topological_node(item.name, item.pose, add_close_nodes=False, update=False, write_map=False)
+            success = self.add_topological_node(item.name, item.pose, False, False, "", "", update=False, write_map=False)
             if not success:
                 return False
 
